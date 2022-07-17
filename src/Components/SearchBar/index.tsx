7ff6efa7c60d9ef0b9c7b8836b7035e76a6bd448
JinJeon <nanyong01@gmail.com>
2022-07-17 15:27:01 +0900

    feat: 한글 자음 또는 모음만 입력되는 경우에 대한 필터 처리

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from 'react';
 
+import { englishTyping, koreanTyping } from 'Util';
+import useInfiniteGoods from 'Hooks/useGoods';
 import { FiltersDispatchContext } from 'Context/FiltersContext';
 import icons from 'static/icons';
 import {
@@ -8,18 +10,58 @@ import {
 	StyledSearchBtn,
 } from './SearchBar.styled';
 
+type TCheckIsKeyword = {
+	keywords: Set<string>;
+	word: string;
+};
+
 const SEARCH_GOODS = '상품명 검색';
 const { search } = icons;
+
+// 들어오는 단어가 "ㄷㄹㄴㅁ", "ㄹㄷ", "ㄱ" 와 같은 경우, 첫 단어만 영어로 바꿔 리턴
+const checkIsAllKorean = (word: string) => {
+	const koreanPart = /[ㄱ-ㅎ|ㅏ-ㅣ]/g;
+	const filteredWord = word.replaceAll(koreanPart, '');
+	if (filteredWord.length) return false;
+
+	const order = koreanTyping.indexOf(word[0]);
+	const englishWord = englishTyping[order];
+	return englishWord;
+};
+
+const checkIsWordIncluded = ({ keywords, word }: TCheckIsKeyword) => {
+	const wordCheckedIsKorean = checkIsAllKorean(word);
+	const comparedWord = wordCheckedIsKorean || word;
+	const keywordsValues = keywords.values();
+	let targetValue = keywordsValues.next().value;
+	let isWordIncluded = false;
+	let wordIncluding;
+
+	while (!isWordIncluded && targetValue) {
+		isWordIncluded = targetValue.includes(comparedWord);
+		if (isWordIncluded) wordIncluding = targetValue;
+		targetValue = keywordsValues.next().value;
+	}
+
+	return wordIncluding;
+};
 
 const SearchBar = () => {
 	const [inputValue, setInputValue] = useState('');
 	const filtersDispatch = useContext(FiltersDispatchContext);
+	const { goodsDataListPages } = useInfiniteGoods();
 
 	const handleSubmitSearchBar = (event: React.FormEvent) => {
 		event.preventDefault();
 		if (!inputValue.length) return;
 
-		filtersDispatch({ type: 'ADD_WORD', content: inputValue });
+		const keywords = goodsDataListPages?.at(-1)?.newGoodsKeywords;
+		if (!keywords) return;
+
+		const wordIncluding = checkIsWordIncluded({ keywords, word: inputValue });
+		if (!wordIncluding) return;
+
+		filtersDispatch({ type: 'ADD_WORD', content: wordIncluding });
 		setInputValue('');
 	};
 
